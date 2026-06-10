a229e8225f02838bd9bf593296f62daa14048d22
vavasoftdev01 <165743409+vavasoftdev01@users.noreply.github.com>
2026-06-10 16:54:23 +0800

    modified/event_endpoints_response

--- app/controllers/event_configs_controller.ts
+++ app/controllers/event_configs_controller.ts
@@ -3,25 +3,26 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class EventConfigsController {
 
-    async getOngoingEvent({}: HttpContext) {
-        const e_ = await EventConfiguration.query().where('isActive', true).first();
-        return {
-            events: e_,
+    async getOngoingEvent({ response }: HttpContext) {
+        const e_ = await EventConfiguration.query().where('isActive', true).orderBy('createdAt', 'desc').first();
+        return response.ok({ success: true, event: e_ })
+    }
+
+    async createEvent({ request, response }: HttpContext) {
+        try {
+            const newEvent = await EventConfiguration.create(request.all())
+            return response.created({ success: true, message: `Event created successfully: ${newEvent.eventName}` })
+        } catch (error) {
+            return response.badRequest({ success: false, error: `Error creating event: ${error}` })
         }
     }
 
-    async createEvent({ request }: HttpContext) {
-        const { name, description, isActive } = request.only(['name', 'description', 'isActive'])
-        const newEvent = await EventConfiguration.create({ eventName: name, eventDescription: description, isActive })
-        return newEvent
-    }
-
-    async updateEvent({ request }: HttpContext) {
+    async updateEvent({ request, response }: HttpContext) {
         const { id, name, description, isActive } = request.only(['id', 'name', 'description', 'isActive'])
         const event = await EventConfiguration.find(id)
 
         if (!event) {
-            return { error: 'Event not found' }
+            return response.notFound({ success: false, error: 'Event not found' })
         }
 
         event.eventName = name
@@ -29,18 +30,18 @@ export default class EventConfigsController {
         event.isActive = isActive
         await event.save()
 
-        return event
+        return response.ok({ success: true, message: `Event updated successfully: ${event.eventName}` })
     }
 
-    async deleteEvent({ request }: HttpContext) {
+    async deleteEvent({ request, response }: HttpContext) {
         const { id } = request.only(['id'])
         const event = await EventConfiguration.find(id)
 
         if (!event) {
-            return { error: 'Event not found' }
+            return response.notFound({ success: false, error: 'Event not found' })
         }
 
         await event.delete()
-        return { message: 'Event deleted successfully' }
+        return response.ok({ success: true, message: 'Event deleted successfully' })
     }
 }
